4e7f799a1f2ba63cddac76e48b9c648ed5004599
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2024-05-01 17:35:18 +0600

    udpate all the code

--- src/pages/Guests.jsx
+++ src/pages/Guests.jsx
@@ -1,20 +1,20 @@
-import CreateGuestForm from '../features/guests/CreateGuestForm';
-import GuestList from '../features/guests/GuestList';
-import Heading from '../ui/Heading';
-import Row from '../ui/Row';
+// import CreateGuestForm from '../features/guests/CreateGuestForm';
+import GuestList from "../features/guests/GuestList";
+import Heading from "../ui/Heading";
+import Row from "../ui/Row";
 
 function Guests() {
-	return (
-		<>
-			<Row>
-				<Heading as='h1'>All Guest List</Heading>
-			</Row>
+  return (
+    <>
+      <Row>
+        <Heading as="h1">All Guest List</Heading>
+      </Row>
 
-			<Row>
-				<GuestList />
-			</Row>
-		</>
-	);
+      <Row>
+        <GuestList />
+      </Row>
+    </>
+  );
 }
 
 export default Guests;
